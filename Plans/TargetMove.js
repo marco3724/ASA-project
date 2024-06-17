f75c36486a93032a9d6214768999cbc0acd414e1
SecondarySkyler <skylerhunter6@gmail.com>
2024-06-17 10:13:05 +0200

    implemented problem gen.

--- Plans/TargetMove.js
+++ Plans/TargetMove.js
@@ -1,5 +1,21 @@
 import {believes,mapConstant,client} from "../Believes.js"
-import * as astar from "../Utility/astar.js"
+import {onlineSolver, PddlProblem} from "@unitn-asa/pddl-client";
+import {Plan} from "./Plan.js"
+import fs from 'fs';
+
+function readFile ( path ) {
+    
+    return new Promise( (res, rej) => {
+
+        fs.readFile( path, 'utf8', (err, data) => {
+            if (err) rej(err)
+            else res(data)
+        })
+
+    })
+
+}
+
 export class TargetMove{
     constructor(intention,intentionRevision){
         this.intention = intention
@@ -8,31 +24,50 @@ export class TargetMove{
         // this.replan = replan
     }
     async execute(){
-        let {intention} = this
+        let {intention} = this // is this necessary?
         console.log("Moving towards target",intention.target)
-        let status,failed_movements=0
-        let {me} = believes
-        let graph = mapConstant.graph
+        let domain = await readFile('./domain.pddl' );
+        let destinationTile = `t_${intention.target.x}_${intention.target.y}`
+        let pddlProblem = new PddlProblem(
+            'move',
+            mapConstant.pddlMapObjects + 'agent1',
+            mapConstant.pddlTiles +  
+            mapConstant.pddlNeighbors + 
+            mapConstant.pddlDeliveryPoints + 
+            `(at agent1 t_${believes.me.x}_${believes.me.y}) ` +
+            `(agent agent1) ` +
+            `(me agent1) `,
+            `at agent1 ${destinationTile}`
+        );
 
-        let current_pos = graph.grid[Math.round(me.x)][Math.round(me.y)];
-        let parcel_node = graph.grid[intention.target.x][intention.target.y];
-        let result = astar.astar.search(graph, current_pos, parcel_node, {diagonal: false});
+        let problem = pddlProblem.toPddlString();
+        console.log(problem);
+        var plan = await onlineSolver(domain, problem);
+        await Plan.pddlExecutor.exec( plan );
 
-        for (let i =0;i < result.length;i++){
-            const direction = result[i].movement
+    //     let status,failed_movements=0
+    //     let {me} = believes
+    //     let graph = mapConstant.graph
 
-            status = await client.move(direction)
-            if(status){
-                console.log("Movement status:",status)
-                believes.me.x = status.x
-                believes.me.y = status.y
-            }
-            else{
-                console.log("Failed movements") 
-                i--
-                failed_movements++
-            }
+    //     let current_pos = graph.grid[Math.round(me.x)][Math.round(me.y)];
+    //     let parcel_node = graph.grid[intention.target.x][intention.target.y];
+    //     let result = astar.astar.search(graph, current_pos, parcel_node, {diagonal: false});
 
-    }
+    //     for (let i =0;i < result.length;i++){
+    //         const direction = result[i].movement
+
+    //         status = await client.move(direction)
+    //         if(status){
+    //             console.log("Movement status:",status)
+    //             believes.me.x = status.x
+    //             believes.me.y = status.y
+    //         }
+    //         else{
+    //             console.log("Failed movements") 
+    //             i--
+    //             failed_movements++
+    //         }
+
+    // }
 }
 }
