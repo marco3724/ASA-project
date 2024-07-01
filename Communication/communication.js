2aad48dff047a48bdbdcefd3a745d039e8c7dcf4
Cristian <skylerhunter6@gmail.com>
2024-07-01 09:39:45 +0200

    handling handshake message

--- Communication/communication.js
+++ Communication/communication.js
@@ -1,21 +1,35 @@
 import { DeliverooApi } from "@unitn-asa/deliveroo-js-client";
+import { Logger } from "../Utility/Logger.js";
 
 export class Communication {
     otherAgent = { id: null };
     constructor(deliverooClient) {
         this.client = deliverooClient;
         this.contactOtherAgent();
+        this.client.onMsg(this.handleMessage);
     }
 
     async contactOtherAgent() {
         // send a message until the other agent responds
         if (this.otherAgent.id == null) {
             setInterval(async () => {
-                await this.client.shout("Hello, are you there?");
+                await this.client.shout({
+                    type: "handshake",
+                    content: "Hello, are you there?"
+                });
             }, 1000);
         } else {
             clearInterval();
         }
+    }
+
+    async handleMessage(id, name, msg, reply) {
+        if (msg.type == "handshake") {
+            this.otherAgent.id = id;
+            await this.client.say(this.otherAgent.id, "Hello, I'm here!");
+        }
+        
+        Logger.logEvent(Logger.logType.COMMUNICATION, Logger.logLevels.INFO, `Received message from ${name} (${id}): ${msg}`);
     }
 
     async sendMessage(message) {
