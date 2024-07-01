5830e3406783b303b5e246a8771168faa9ce9924
Cristian <skylerhunter6@gmail.com>
2024-07-01 09:39:11 +0200

    added communication logtype

--- Utility/Logger.js
+++ Utility/Logger.js
@@ -8,6 +8,7 @@ export const Logger = {
         INTENTION: '\x1b[32mINTENTION \x1b[0m',
         PLAN: '\x1b[33mPLAN \x1b[0m',
         BELIEVES: '\x1b[34mBELIEVES \x1b[0m',
+        COMMUNICATION: '\x1b[35mCOMMUNICATION \x1b[0m',
     },
     logEvent (type, level, message) {
 
